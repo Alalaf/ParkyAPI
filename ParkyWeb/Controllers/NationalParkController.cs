5d59d8fc3a3edcb1ed7d288c0a4c65d12894df6e
ahmedalalaf <ahmedalalaf@gmail.com>
2020-01-25 14:05:33 -0800

    Add NationalPark controller and View

---
+++ ParkyWeb/Controllers/NationalParkController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+
+namespace ParkyWeb.Controllers
+{
+    public class NationalParkController : Controller
+    {
+        private readonly INationalParkRepository _nprepo;
+        public NationalParkController(INationalParkRepository nprepo)
+        {
+            _nprepo = nprepo;
+        }
+        public IActionResult Index()
+        {
+            return View(new NationalPark() { });
+        }
+
+        public async Task<IActionResult> GetALLNationalPark()
+        {
+            return Json(new { data = await _nprepo.GetAllAsync(SD.NationalParkAPIPath) });
+        }
+    }
+}
